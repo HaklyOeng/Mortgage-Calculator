ea69b6cc0b705aa1e626bc9974050eee79bb54e5
Hakly Oeng <helihaung@gmail.com>
2023-10-24 15:36:50 -0400

    fix month

--- js/site.js
+++ js/site.js
@@ -1,20 +1,36 @@
 function getValues() {
     let loanAmount = document.getElementById('loanAmount').value;
     let months = document.getElementById('months').value;
-    let interestRate = document.getElementById('interestRate').value;    
+    let interestRate = document.getElementById('interestRate').value;
 
     loanAmount = parseInt(loanAmount);
     months = parseInt(months);
-    interestRate = parseInt(interestRate);
+    interestRate = parseFloat(interestRate);
 
-    if (loanAmount <= 0 && months <= 0 && interestRate <= 0) {
+    //Total Principle
+    document.querySelector('#card-principal').textContent = `$ ${loanAmount.toFixed(2)}`;
+    let input = {
+        loanAmount: loanAmount,
+        term: months,
+        rate: interestRate
+    }
+    /*
+    if (isNan(priniciple)) {
+        return an error
+    } */
+
+    if (Number.isInteger(loanAmount) && Number.isInteger(months) && Number.isInteger(interestRate)) {
+
+        let results = loanCalculate(input);
+        displayResults(results);      
+    } else if (loanAmount <= 0 && months <= 0 && interestRate <= 0) {
         return Swal.fire({
             icon: 'error',
             backdrop: false,
             title: 'Attention',
             text: "Every entrie can't be 0 or less"
         });
-    } else if (loanAmount <= 0 && months > 0 && interestRate > 0){
+    } else if (loanAmount <= 0 && months > 0 && interestRate > 0) {
         return Swal.fire({
             icon: 'error',
             backdrop: false,
@@ -35,18 +51,16 @@ function getValues() {
             title: 'Attention',
             text: 'Please check the Interest Rate and enter an a Rate that is more than 0'
         });
+    } else {        
+        Swal.fire({
+            icon: 'error',
+            title: 'Attention',
+            text: 'Please enter valid numbers.'
+        });
     }
 
-    //Total Principle
-    document.querySelector('#card-principal').textContent = `$ ${loanAmount.toFixed(2)}`;
-    let input = {
-        loanAmount: loanAmount,
-        term: months,
-        rate: interestRate
-    }
 
-    let results = loanCalculate(input);
-    displayResults(results);
+
 
 }
 
@@ -60,31 +74,31 @@ function loanCalculate(input) {
     let monthlyPayment = 0;
     let remainingBalance = loan;
     let addingInterest = 0;
-    let totalCost = 0;   
+    let totalCost = 0;
 
 
     for (i = 0; i < months; i++) {
 
-         monthlyPayment = (loan * (rate / 1200) / (1 - Math.pow((1 + rate / 1200), (- months))));
-        let interestPayment = (remainingBalance * rate / 1200);        
+        monthlyPayment = (loan * (rate / 1200) / (1 - Math.pow((1 + rate / 1200), (- months))));
+        let interestPayment = (remainingBalance * rate / 1200);
         let principalPayment = (monthlyPayment - interestPayment);
 
         if (remainingBalance == loan || addingInterest < interestPayment || totalCost < monthlyPayment) {
             remainingBalance = (loan - principalPayment);
             addingInterest = interestPayment;
-            totalCost = monthlyPayment    ;        
+            totalCost = monthlyPayment;
         } else {
             (remainingBalance - principalPayment);
             addingInterest += interestPayment;
             totalCost += monthlyPayment;
         }
-        
+
 
 
         let tableRow = document.createElement('tr');
 
         let month = document.createElement('td');
-        month.innerText = months - i;
+        month.innerText = i + 1;
         tableRow.appendChild(month);
 
         let payment = document.createElement('td');
@@ -126,3 +140,28 @@ function displayResults(input) {
     document.querySelector('#card-totalInterest').textContent = `$ ${totalInterest}`;
     document.querySelector('#card-total').textContent = `$ ${totalCost}`;
 }
+
+/*
+function displayPayment(paymentsArr) {
+
+    const formatOptions = {
+        style = 'currency',
+        currency = 'USD'
+    };
+    const tableRowTemplate = document.getElementById('monthlyPaymentTemplate');
+
+    paymentsArr.forEach(paynebt => {
+        let tableRow = tableRowTemplate.content.cloneNode(true);
+
+        let tableCells = tableRow.querrySelectorAll('td'); // give you an array of element
+
+        tableCell[0].textContent = payment.month;
+        tableCell[1].textContent = payment.monthlyPayment.toLocalStrong('en-US', formatOptions);
+        tableCell[2].textContent = payment.principal;
+        tableCell[3].textContent = payment.interest;
+        tableCell[4].textContent = payment.totalInterest;
+        tableCell[5].textContent = Math.abs(payment.balance);
+
+    })
+}
+*/
